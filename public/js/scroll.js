a86efaa356b8d026afcf5954d0e261b273b702f4
Sebastian Bejm <125sbejm@gmail.com>
2019-05-09 12:25:18 -0700

    moved map code to map.js, added search icon

--- public/js/scroll.js
+++
@@ -1,9 +0,0 @@
- //Goes to the top page when hit refresh.
-        window.onbeforeunload = function () {
-            window.scrollTo(0, 0);
-        }
-
-        //button select functions
-        $(".btn-group > .btn").click(function(){
-            $(this).addClass("active").siblings().removeClass("active");
-        });
