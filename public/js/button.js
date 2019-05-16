a64db24a20f8ae07b520553b05ec104e7da650ef
Rammel Razon <rammel1012@gmail.com>
2019-05-16 12:45:35 -0700

    button

---
+++ public/js/button.js
@@ -0,0 +1,66 @@
+var myFirebase = firebase.database();
+
+var wasteButton = document.getElementById("waste");
+var recyclingButton = document.getElementById("recycling");
+var compostButton = document.getElementById("compost");
+var paperButton = document.getElementById("paper");
+
+wasteButton.addEventListener("click", function(){
+   
+   var ref = myFirebase.ref("BldngInfo");
+    ref.on("child_added", function(snapshot){
+       var x = snapshot.val();
+        
+        console.log("BuildingNo " + snapshot.key);
+        console.log("Latitute " + x.BldngLat);
+        console.log("Longitude " + x.BldngLong)
+        console.log("Garbage? " + x.waste)
+    });
+});
+
+recyclingButton.addEventListener("click", function(){
+   var ref = myFirebase.ref("BldngInfo");
+    ref.on("child_added", function(snapshot){
+       var x = snapshot.val();
+        
+        console.log("BuildingNo " + snapshot.key);
+        console.log("Latitute " + x.BldngLat);
+        console.log("Longitude " + x.BldngLong)
+        console.log("Recyling " + x.recycling)
+    });
+});
+
+
+compostButton.addEventListener("click", function(){
+   
+   var ref = myFirebase.ref("BldngInfo");
+   
+    ref.on("child_added", function(snapshot){
+       
+       var x = snapshot.val();
+        
+        console.log("BuildingNo " + snapshot.key);
+        console.log("Latitute " + x.BldngLat);
+        console.log("Longitude " + x.BldngLong)
+        console.log("Compost? " + x.organic)
+    });
+});
+
+
+paperButton.addEventListener("click", function(){
+   
+   var ref = myFirebase.ref("BldngInfo");
+   
+    ref.on("child_added", function(snapshot){
+       
+       var x = snapshot.val();
+        console.log("BuildingNo " + snapshot.key);
+        console.log("Latitute " + x.BldngLat);
+        console.log("Longitude " + x.BldngLong)
+        console.log("Paper " + x.paper)
+    });
+});
+
+
+
+
